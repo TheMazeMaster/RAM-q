e0027af47f83c537d7096d3ee3ed8eef802f046d
TheMazeMaster <the_maze_master@yahoo.com>
2025-04-03 20:22:43 +0100

    Create instincts.js

---
+++ data/instincts.js
@@ -0,0 +1,13 @@
+export const instincts = [
+  {
+    name: "Avoidant",
+    scale: [
+      "Vanishes when stressed",
+      "Pulls away quietly",
+      "Balances space with connection",
+      "Stays emotionally close",
+      "Clings, needs immediate closeness"
+    ]
+  },
+  // Repeat for all 10
+];

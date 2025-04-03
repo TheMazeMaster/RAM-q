69d432ed88c5a2b063e91d2e8d2b833f425183b3
TheMazeMaster <the_maze_master@yahoo.com>
2025-04-03 20:19:40 +0100

    Create themes.js

---
+++ data/themes.js
@@ -0,0 +1,13 @@
+export const themes = [
+  {
+    id: 1,
+    title: "Conflict & Communication",
+    questions: {
+      A: "When we argue, my typical approach is…",
+      B: "When we argue, my partner typically…",
+      C: "What I wish they did for themselves during conflict is…",
+      D: "What I wish they did for me/us during conflict is…"
+    }
+  },
+  // Repeat for 9 more
+];
